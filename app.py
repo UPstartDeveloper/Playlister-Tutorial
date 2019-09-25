7b36ac811f71506677e8f42771e7d6ab38512136
= <zainr7989@gmail.com>
2019-09-24 17:56:50 -0700

    Finish page 2.

--- app.py
+++ app.py
@@ -6,8 +6,19 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     """Return homepage."""
-    return render_template('home.html', msg='Flask is Cool!')
-    # return 'Hello, world!'
+    # OUR MOCK ARRAY OF PROJECTS
+    playlists = [
+        {'title': 'Cat Videos', 'description': 'Cats acting weird'},
+        {'title': '80\'s Music', 'description': 'Don\'t stop believing!'},
+        {'title': 'Spirituality', 'description': 'Majalis-e-Aza'}
+        ]
+    return render_template('playlists_index.html', playlists=playlists)
+
+
+@app.route('/')
+def playlists_index():
+    """Show all playlists."""
+    return render_template('playlists_index.html', playlists=playlists)
 
 
 if __name__ == '__main__':
