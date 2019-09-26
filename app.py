5c3ef61cba8b86e91e4717f063f59f6c02bc798e
= <zainr7989@gmail.com>
2019-09-25 18:10:17 -0700

    Fix syntax errors.

--- app.py
+++ app.py
@@ -58,7 +58,7 @@ def playlists_show(playlist_id):
 
 
 @app.route('/playlists/<playlist_id>/edit')
-def playlists_edit"(playlist_id):
+def playlists_edit(playlist_id):
     """Show the edit form for a playlist."""
     playlist = playlists.find_one({'_id': ObjectId(playlist_id)})
     # video_links = '\n'.join(playlist.get('videos'))
@@ -78,10 +78,9 @@ def playlists_update(playlist_id):
         {'_id': ObjectId(playlist_id)},
         {'$set': updated_playlist})
     return redirect(url_for('playlists_show', playlist_id=playlist_id))
-    )
 
 
-@app.route('play')
+# @app.route('play')
 
 
 if __name__ == '__main__':
