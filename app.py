a7125a8c401be079f6ca4430c38d5ac3c97b0970
= <zainr7989@gmail.com>
2019-09-27 16:21:03 -0700

    Users can see comments.

--- app.py
+++ app.py
@@ -3,8 +3,8 @@ from pymongo import MongoClient
 from bson.objectid import ObjectId
 import os
 
-host = os.environ.get('MONGODB_URI')  # point to the mongodb URI if it exists
-# client = MongoClient(host=host)
+# point to the mongodb URI if it exists
+host = os.environ.get('MONGODB_URI', 'mongodb://localhost:27017/Playlister')
 client = MongoClient(host=f'{host}?retryWrites=false')
 db = client.get_default_database()
 playlists = db.playlists
