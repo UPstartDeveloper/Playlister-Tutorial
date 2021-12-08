55728150e26d3775dd7a3deeb54f14e6c50097fc
= <zainr7989@gmail.com>
2021-12-07 21:48:46 -0500

    Register Playlists Blueprint.

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,9 @@
 from flask import Flask
+from app.playlists.routes import playlists_bp
 
 
 def create_app():
+    """Init the app, and all the same routes as we had before."""
     app = Flask(__name__)
+    app.register_blueprint(playlists_bp)
     return app
